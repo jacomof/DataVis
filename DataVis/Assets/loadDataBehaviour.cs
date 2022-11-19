5d82ea977e858dfebd232cf9d65b161bfe3d8b9e
Joaquin Figueira <joaquinfigueira85@gmail.com>
2022-11-20 00:21:01 +0100

    Finishing first version of DataSet object. It is to be attached to a visualization object for easy access to a data table in csv format.

--- DataVis/Assets/loadDataBehaviour.cs
+++ DataVis/Assets/loadDataBehaviour.cs
@@ -1,16 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.VisualBasic.FileIO;
 using Microsoft.Data.Analysis;
+using System.IO;
+using UnityEditor;
 
 public class loadDataBehaviour : MonoBehaviour
 {
+    [SerializeField] private TextAsset dataAsset;
     // Start is called before the first frame update
     void Start()
     {
-        var path="/home/joaquin/DataVis/DataVis/Assets/Data/test_data.csv";
-        TextFieldParser csvParser = new TextFieldParser(path);
+        
+        TextFieldParser csvParser = new TextFieldParser(AssetDatabase.GetAssetPath(dataAsset));
         csvParser.CommentTokens = new string[] { "#" };
         csvParser.SetDelimiters(new string[] {","});
         csvParser.HasFieldsEnclosedInQuotes = true;
@@ -21,8 +23,15 @@ public class loadDataBehaviour : MonoBehaviour
         {
             string[] fields = csvParser.ReadFields();
         }
-    
+        DataFrame readData = DataFrame.LoadCsvFromString(dataAsset.ToString(), header:false);
         Debug.Log("File read!");
+        bool[] arrayBool = {false,true,false,true,true,false};
+        //Debug.Log(readData.ToString());
+        DataFrameColumnCollection dfc = readData.Columns;
+        DataFrameColumn[] dfcArray = new StringDataFrameColumn[]{(StringDataFrameColumn) dfc["Column0"], (StringDataFrameColumn) dfc["Column1"]};
+        DataFrame subData = new DataFrame(dfcArray);
+        Debug.Log((readData[arrayBool]).ToString());
+        Debug.Log(subData.ToString());
     }
 
     // Update is called once per frame
