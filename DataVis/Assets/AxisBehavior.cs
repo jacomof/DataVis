6525a27905f82535225d42dbfe4429332bd65192
Joaquin Figueira <joaquinfigueira85@gmail.com>
2022-11-22 23:27:35 +0100

    Finishing up first version of axis.

--- DataVis/Assets/AxisBehavior.cs
+++ DataVis/Assets/AxisBehavior.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AxisBehavior : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class AxisBehavior : MonoBehaviour
     } 
 
     public AxisId MyAxisId;
-    private float _Unit = 1.0f;
+    private float _Unit = 2.0f;
 
     private float _ValueUnit = 1.0f;
 
@@ -24,6 +25,7 @@ public class AxisBehavior : MonoBehaviour
     void Start()
     {
         _Size = VisualizationBehavior.PLANE_SIZE;
+        _ValueUnit = _ValueUnit*_Unit;
         if(MyAxisId == AxisId.X)
             _initializeXAxis();
         else if(MyAxisId == AxisId.Y)
@@ -38,23 +40,45 @@ public class AxisBehavior : MonoBehaviour
         int _numStripes = (int) Mathf.Floor(_Size/_Unit);
         Vector3 _stripeDif = new Vector3(_Unit, 0, 0);
         var _lineRenderer = gameObject.GetComponent<LineRenderer>();
-        _lineRenderer.SetPosition(1, _stripeDif*_Size);
-        var _stripePos = _stripeDif;
-        for (int i = 0; i < _numStripes; ++i){
+        _lineRenderer.SetPosition(1, _stripeDif*_Size); //Sets position of the end point of the line to the size of the visualization volume
+        var _stripePos = new Vector3(0,0,0);
+        var _valueUnitIter = 0.0f;
 
+        for (int i = 0; i < _numStripes; ++i){
+            
             var _textStripe = GameObject.Instantiate(StripeText, gameObject.transform);
-            _stripePos += _stripePos+_stripeDif;
-            _textStripe.transform.position = _stripePos;
+            _stripePos += _stripeDif;
+            _valueUnitIter += _ValueUnit;
+            var _textComp = _textStripe.GetComponent<TextMeshPro>();
+            var _transform = _textStripe.GetComponent<RectTransform>();
+            _textComp.text = _valueUnitIter.ToString();
+            _transform.localPosition = _stripePos;
 
         }
 
-        throw new NotImplementedException();
     }
 
     private void _initializeYAxis()
     {
 
-        throw new NotImplementedException();
+        int _numStripes = (int) Mathf.Floor(_Size/_Unit);
+        Vector3 _stripeDif = new Vector3(0, _Unit, 0);
+        var _lineRenderer = gameObject.GetComponent<LineRenderer>();
+        _lineRenderer.SetPosition(1, _stripeDif*_Size); //Sets position of the end point of the line to the size of the visualization volume
+        var _stripePos = new Vector3(-0.2f,0,0);
+        var _valueUnitIter = 0.0f;
+
+        for (int i = 0; i < _numStripes; ++i){
+            
+            var _textStripe = GameObject.Instantiate(StripeText, gameObject.transform);
+            _stripePos += _stripeDif;
+            _valueUnitIter += _ValueUnit;
+            var _textComp = _textStripe.GetComponent<TextMeshPro>();
+            var _transform = _textStripe.GetComponent<RectTransform>();
+            _textComp.text = _valueUnitIter.ToString();
+            _transform.localPosition = _stripePos;
+
+        }
     }
 
     
@@ -62,7 +86,29 @@ public class AxisBehavior : MonoBehaviour
     private void _initializeZAxis()
     {
 
+        int _numStripes = (int) Mathf.Floor(_Size/_Unit);
+        Vector3 _stripeDif = new Vector3(0, 0, _Unit);
+        var _lineRenderer = gameObject.GetComponent<LineRenderer>();
+        _lineRenderer.SetPosition(1, _stripeDif*_Size); //Sets position of the end point of the line to the size of the visualization volume
+        var _stripePos = new Vector3(0,0,0);
+        var _valueUnitIter = 0.0f;
+
+        for (int i = 0; i < _numStripes; ++i){
+            
+            var _textStripe = GameObject.Instantiate(StripeText, gameObject.transform);
+            _stripePos += _stripeDif;
+            _valueUnitIter += _ValueUnit;
+            var _textComp = _textStripe.GetComponent<TextMeshPro>();
+            var _transform = _textStripe.GetComponent<RectTransform>();
+            _textComp.text = _valueUnitIter.ToString();
+            _transform.localPosition = _stripePos;
+            _transform.localEulerAngles = new Vector3(0, 90.0f, 0);
+
+        }
+
     }
+
+    
 
 
     // Update is called once per frame
